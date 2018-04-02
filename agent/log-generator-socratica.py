406edf611fb4cb22c7bde1c73934b4709579e143
FilipSavic95 <filipsavic1995@yahoo.com>
2018-04-03 01:33:26 +0200

    Added MAC address to the generated log.

--- agent/log-generator-socratica.py
+++ agent/log-generator-socratica.py
@@ -1,9 +1,22 @@
 # tutorial  : > :  https://www.youtube.com/watch?v=g8nQ90Hk328
 import logging
+from uuid import getnode as get_mac
 
+
+def get_app():
+    from random import randint
+    app_list = ['student-service', 'professor-service', 'referent-service']
+    return app_list[randint(0, 2)]
 
 def main():
-    log_format = "%(asctime)s  :  %(levelname)s  -  %(message)s"
+    #     timestamp
+    #     mac address
+    #     app
+    #     severity
+    mac = get_mac()
+    mac = ':'.join(("%012X" % mac)[i:i + 2] for i in range(0, 12, 2))
+
+    log_format = "%(asctime)s - " + mac + " : " + get_app() + " :  %(levelname)s - %(message)s"
     logging.basicConfig(
         filename="generated-logs/socratica-log.log",
         datefmt='%d-%m-%Y %H:%M:%S',
@@ -11,7 +24,7 @@ def main():
         format=log_format,
         filemode='a')
 
-    logger = logging.getLogger()
+    logger = logging.getLogger()  # docs  : > :  https://docs.python.org/3/library/logging.html
 
     logger.debug("this is a harmless debug message.")
     logger.info("this is a informing message")
--- agent/log-reader-SO.py
+++ agent/log-reader-SO.py
@@ -1,24 +1,29 @@
+# Windows Logs - System, Application and Security : > :
+# https://docs.microsoft.com/en-us/previous-versions/windows/it-pro/windows-server-2008-R2-and-2008/cc722404%28v%3dws.10%29
+
 # StackOverflow question  : > :  https://stackoverflow.com/questions/11219213/read-specific-windows-event-log-event
 # Docs and possible improvent (an application which looks for all events for a server since a certain time) : > :  http://timgolden.me.uk/pywin32-docs/Windows_NT_Eventlog.html
 
+# win32evtlog  : > :  https://github.com/wuxc/pywin32doc/blob/master/md/win32evtlog.md
 import win32evtlog # requires pywin32 pre-installed (pywin32: https://github.com/mhammond/pywin32 )
 
 def main():
     server = 'localhost' # name of the target computer to get event logs
-    logtype = 'System' # 'Application' # 'Security'
+    logtype = 'Security'  # 'Application' # 'Security' # 'System'
     hand = win32evtlog.OpenEventLog(server,logtype)
     flags = win32evtlog.EVENTLOG_FORWARDS_READ|win32evtlog.EVENTLOG_SEQUENTIAL_READ  # change EVENTLOG_FORWARDS_READ to EVENTLOG_BACKWARDS_READ in order to get the Events listed from the most recent one down to the first one ever logged
     total = win32evtlog.GetNumberOfEventLogRecords(hand)
 
-    event_type_dict = {1: '1-Error', 2: 'Warning', 4: '4-Information', 8: '8-Audit success', 16: '16-Audit failure'}
+    event_type_dict = {1: '1-Error', 2: 'Warsning', 4: '4-Information', 8: '8-Audit success', 16: '16-Audit failure'}
 
     while True:
-        events = win32evtlog.ReadEventLog(hand, flags,0)
+        events = win32evtlog.ReadEventLog(hand, flags, 0)
         if events:
-            for event in events:
+            for event in events:  # event attributes  : > :  https://github.com/wuxc/pywin32doc/blob/master/md/PyEventLogRecord.md
+                #print('Sid', event.Sid)  # SecurityIDentifiers  : > :  https://msdn.microsoft.com/en-us/library/windows/desktop/aa379649%28v=vs.85%29.aspx
                 print('Event Category:', event.EventCategory)
                 print('Time Generated:', event.TimeGenerated)
-                print('Source Name:', event.SourceName)
+                print('Source Name:', event.SourceName)  # name of the software that logs the event
                 print('Event ID:', event.EventID)
                 print('Event Type:', event_type_dict[event.EventType])
                 data = event.StringInserts
