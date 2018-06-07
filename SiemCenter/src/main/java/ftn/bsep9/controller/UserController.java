9b02f0b512b810258f4a376dc840f49addf5c5aa
FilipSavic95 <filipsavic1995@yahoo.com>
2018-06-07 12:27:50 +0200

    Returning JWT as response body, not header.

--- SiemCenter/src/main/java/ftn/bsep9/controller/UserController.java
+++ SiemCenter/src/main/java/ftn/bsep9/controller/UserController.java
@@ -3,8 +3,6 @@ package ftn.bsep9.controller;
 import ftn.bsep9.model.User;
 import ftn.bsep9.security.TokenUtils;
 import ftn.bsep9.service.UserService;
-import org.apache.http.protocol.HTTP;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -13,13 +11,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
-import javax.servlet.http.HttpSession;
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -62,20 +54,14 @@ public class UserController {
     @PostMapping(value = "/login")
     public ResponseEntity<String> login(@RequestBody User user) {
         try {
-            System.out.println("\n LOGIN POST");
-            System.out.println(user.getUsername());
-            System.out.println(user.getPassword());
             UsernamePasswordAuthenticationToken token = new UsernamePasswordAuthenticationToken(user.getUsername(), user.getPassword());
             Authentication authentication = authenticationManager.authenticate(token);
             SecurityContextHolder.getContext().setAuthentication(authentication);
 
             UserDetails details = userService.loadUserByUsername(user.getUsername());
-            HttpHeaders headers = new HttpHeaders();
-            headers.set("X-Auth-Token", tokenUtils.generateToken(details));
-
-            return new ResponseEntity<>(headers, HttpStatus.OK);
+            return new ResponseEntity<>(tokenUtils.generateToken(details), HttpStatus.OK);
         } catch (Exception ex) {
-            return new ResponseEntity<>("Login failed", HttpStatus.UNAUTHORIZED);
+            return new ResponseEntity<>("Login failed. Bad username or password.", HttpStatus.UNAUTHORIZED);
         }
     }
 
@@ -84,9 +70,9 @@ public class UserController {
 ////    @PreAuthorize("hasAnyAuthority('OPERATOR', 'ADMIN')")
 //    public ResponseEntity<String> changePassword(@RequestParam Map<String, String> params) {
 //        if (userService.changePassword(params)) {
-//            return "logs-view";
+//            return new ResponseEntity<String>(HttpStatus.OK, "asd");
 //        }
-//        return new ResponseEntity<String>(HttpStatus.OK, 'asd');
+//        return new ResponseEntity<String>(HttpStatus.OK, "asd");
 //    }
 
 
