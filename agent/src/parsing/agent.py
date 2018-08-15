62e26b52208e0e49a67efa12f81d2bbe72712289
FilipSavic95 <filipsavic1995@yahoo.com>
2018-08-15 22:42:26 +0200

    Added date, time, service and ID parsing. Agent starts appropriate os script.

--- agent/src/parsing/agent.py
+++ agent/src/parsing/agent.py
@@ -1,10 +1,17 @@
 from threading import Thread
+import platform
 
 import application_logs
-import windows_logs
+
 
 if __name__ == '__main__':
-    thread1 = Thread(target=windows_logs.main)
+    print platform.system().upper()
+    if platform.system().upper()=='WINDOWS':
+        import windows_logs
+        thread1 = Thread(target=windows_logs.main)
+    if platform.system().upper()=='LINUX':
+        import linux_logs
+        thread1 = Thread(target=linux_logs.main)
     thread2 = Thread(target=application_logs.main)
 
     thread1.start()
--- agent/src/parsing/linux_logs.py
+++ agent/src/parsing/linux_logs.py
@@ -1,20 +1,30 @@
 # https://stackoverflow.com/questions/11908919/python-parsing-log-file-to-extract-events-in-real-time
 # http://www.dabeaz.com/generators/follow.py
 
-from datetime import datetime
-import time
+import json
 import os
 
+from datetime import datetime
+import time
+
+from requests_util import open_session, send_log
+
+
+#TODO - #1 (main)
+# 1) da struktura fajla lici na windows_logs
 
 month_dict = {
-        'Jan': '01'', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06',
+        'Jan': '01', 'Feb': '02', 'Mar': '03', 'Apr': '04', 'May': '05', 'Jun': '06',
         'Jul': '07', 'Aug': '08', 'Sep': '09', 'Oct': '10', 'Nov': '11', 'Dec': '12'
-                }
-
-warning_words = ['warning', 'warn', 'reject', 'rejected']
-error_words = ['error', 'err', 'bad']
+        }
 
 current_year = datetime.now().year
+
+
+def read_config(config_path):
+    with open(config_path) as f:
+        configuration = json.load(f)
+    return configuration["OS"]
 
 
 def get_mac_address(interface='eth0'):
@@ -32,25 +42,26 @@ def follow(thefile):
         line = thefile.readline()
         if not line:
             time.sleep(1)
-            time.ctime()
             continue
         yield line
 
 
-def decide_severity(log_text):
-    for warning_word in warning_words:
-        if log_text.find(warning_word):
-            return 'WARNING'
-    for error_word in error_words:
-        if log_text.find(error_word):
-            return 'ERROR'
-
-
-#TODO
-#def get_service(service_id_word):
-
-#TODO
-#def get_id(service_id_word):
+def get_service_and_id(service_id_word):
+    ''' service_id_word examples: 'dbus[502]:'  or: 'pkexec:' '''
+    print 'print service_id_word'
+    print service_id_word
+    bracket_index = service_id_word.find('[')
+    
+    if bracket_index != -1:
+        service = service_id_word[ : bracket_index]
+        id = service_id_word[bracket_index : -1]
+    else:
+        service = service_id_word[ : -1]
+        id = None
+    
+    print 'print service, id'
+    print service, id
+    return service, id
 
 
 def process_line(line):
@@ -61,7 +72,8 @@ def process_line(line):
         
     #Aug 14 14:51:36 osboxes dbus[502]: [system] Rejected send message, 7 matched rules; type='method_return', sender=':1.15' (uid=0 pid=1101 comm='/usr/sbin/dnsmasq --no-resolv --keep-in-foreground') interface='(unset)' member='(unset)' error name='(unset)' requested_reply='0' destination=':1.6' (uid=0 pid=939 comm='NetworkManager ')
 
-    #Aug 12 11:17:01 osboxes CRON[3438]: pam_unix(cron:session): session opened for user root by (uid=0)
+    #Aug 14 15:39:48 osboxes pkexec: pam_unix(polkit-1:session): session opened for user root by (uid=1000)
+    #Aug 14 15:39:48 osboxes pkexec[23562]: osboxes: Executing command [USER=root] [TTY=unknown] [CWD=/home/osboxes] [COMMAND=/usr/lib/update-notifier/package-system-locked]
     #Aug 12 11:17:01 osboxes CRON[3438]: pam_unix(cron:session): session closed for user root
 
     #29-06-2018 17:08:15 	F4:4D:45:7D:36:FD 	student-service : 	INFO - [1530284895058] Logged out successfully
@@ -73,14 +85,17 @@ def process_line(line):
     mac_address = get_mac_address()
     #system name = words[3] (osboxes)
     #TODO
-    #service = get_service(words[4])
+    service, id = get_service_and_id(words[4])
     #severity = decide_severity(log_text.lower())
-    #id = get_id(words[4])
     
     print '    Timestamp:'
     print '    ' + timestamp
     print '    MAC Address:'
     print '    ' + mac_address
+    print '    Service'
+    print '    ' + service
+    print '    id'
+    print '    ' + id
     print '    -------------------------------------'
 
     #~ syslog_message = '{timestamp} \t{mac_address} \t{service} : \t{severity} - [{id}] {message}'.format(
@@ -95,6 +110,9 @@ def process_line(line):
 
 def main():
     configuration = read_config("config.json")
+    
+    session = open_session()
+    
     logfile = open('/var/log/auth.log', 'r')
     #logfile = open('/tmp/test_log', 'r')
     loglines = follow(logfile)
